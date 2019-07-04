ec20de9c89050ff888ff923e9245280e64b51d65
lxl <lxl_829@163.com>
2019-07-04 17:00:13 +0800

    订单模块完成，支付模块回调失败 二维码上传不成功

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -30,8 +30,8 @@ public class UserController<controller> {
      * @param session
      * @return
      */
-    @RequestMapping(value = "login.do",method = RequestMethod.POST)
     //在返回时通过spring MVC的插件将返回值自动序列化成json格式
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session){
         //service-->mybatis-->dao
